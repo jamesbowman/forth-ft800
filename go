8967161362975e404b7b03d4ff3c73191386189e
jamesbowman <jamesb@excamera.com>
2015-01-27 19:39:26 -0800

    Initial import

---
+++ go
@@ -0,0 +1,1 @@
+gforth standard.fs smoketest.fs
---
+++ smoketest.fs
@@ -0,0 +1,42 @@
+\ Smoke test for gd2.fs
+\ This file defines the extra words needed by gd2.fs,
+\ and compiles it.
+
+: LOCALWORDS
+    get-current
+    get-order wordlist swap 1+ set-order definitions
+;
+
+: PUBLICWORDS
+    set-current
+;
+
+: DONEWORDS
+    previous
+;
+
+: gd2-spi-init
+;
+
+: gd2-sel
+;
+
+: gd2-unsel
+;
+
+: >spi
+    drop
+;
+
+: spi>
+    0
+;
+
+: uw@ ( a -- x )
+    dup c@ swap 1+ c@ 8 lshift +
+;
+
+: w@    uw@ ;
+
+s" gd2.fs" included
+bye
