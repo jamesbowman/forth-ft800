0d3287c46648cad21335d1b28357239aaed31711
jamesbowman <jamesb@excamera.com>
2015-03-28 09:00:50 -0700

    Make into a loop

--- demos/helloworld.fs
+++ demos/helloworld.fs
@@ -1,8 +1,10 @@
 : helloworld
     GD.init
 
-    $103000 GD.ClearColorRGB#
-    GD.Clear
-    240 136 31 GD.OPT_CENTER s" Hello world" GD.cmd_text
-    GD.swap
+    begin
+        $103000 GD.ClearColorRGB#
+        GD.Clear
+        240 136 31 GD.OPT_CENTER s" Hello world" GD.cmd_text
+        GD.swap
+    again
 ;
